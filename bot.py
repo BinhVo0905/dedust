6296f543ec0040a171987473a227082695c0834a
vocongbinh <binh93phudong@gmail.com>
2024-07-16 22:55:22 +0700

    dev: default buy 0.1 TON and sell all current balance

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ mnemonics = ["easy", "sunny", "senior", "ritual", "deputy", "garden", "circle", 
              "topple", "rubber", "rubber", "luggage", "combine", "version", "educate", "pilot", "journey",
              "start", "busy", "matter", "leopard", "outside"]
 
-async def buy(amount, token_address):
+async def buy(token_address, amount=0.1):
     provider = LiteBalancer.from_mainnet_config(1)
     await provider.start_up()
     wallet = await WalletV4R2.from_mnemonic(provider=provider, mnemonics=mnemonics) 
@@ -17,24 +17,30 @@ async def buy(amount, token_address):
     TOKEN = Asset.jetton(token_address)
     pool = await Factory.get_pool(pool_type=PoolType.VOLATILE, assets=[TON, TOKEN], provider=provider)
     swap_params = SwapParams(deadline=int(time.time() + 60 * 5), recipient_address=wallet.address)
-    swap_amount = int(float(amount) * 1e9)
+    if amount == None:
+        swap_amount = int(float(0.1) * 1e9)
+    else: swap_amount = int(float(amount) * 1e9)
+    print(swap_amount)
     swap = VaultNative.create_swap_payload(amount=swap_amount, pool_address=pool.address, swap_params=swap_params)
-    swap_amount += int(0.25 * 1e9)  # 0.25 = gas_value
+    swap_amount += int(0.25 * 1e9) 
     await wallet.transfer(destination="EQDa4VOnTYlLvDJ0gZjNYm5PXfSmmtL6Vs6A_CZEtXCNICq_", amount=swap_amount, body=swap)
     await provider.close_all()
 
-async def sell(amount, token_address):
+async def sell(token_address, amount=None):
     provider = LiteBalancer.from_mainnet_config(1)
     await provider.start_up()
     wallet = await WalletV4R2.from_mnemonic(provider=provider, mnemonics=mnemonics)
     TON = Asset.native()
     TOKEN = Asset.jetton(token_address)
     pool = await Factory.get_pool(pool_type=PoolType.VOLATILE, assets=[TON, TOKEN], provider=provider)
-    
     token_vault = await Factory.get_jetton_vault(token_address, provider=provider)
     token_root = JettonRoot.create_from_address(token_address)
     token_wallet = await JettonRoot.get_wallet(token_root, wallet.address, provider)
-    swap_amount = int(float(amount) * 1e9)
+    if amount == None:
+        swap_amount = await token_wallet.get_balance(provider=provider)
+    else:
+        swap_amount = int(float(amount) * 1e9) 
+    print(swap_amount)
     swap = token_wallet.create_transfer_payload(
         destination=token_vault.address,
         amount=swap_amount,
@@ -44,22 +50,25 @@ async def sell(amount, token_address):
     )
     await wallet.transfer(destination=token_wallet.address, amount=int(0.3*1e9), body=swap)
     await provider.close_all()
-
+    
 async def main(action, amount, token_address):
     if action == "buy":
-        await buy(amount, token_address)
+        await buy(token_address, amount)
     elif action == "sell":
-        await sell(amount, token_address)
+        await sell(token_address,amount)
     else:
-        print("Action không hợp lệ. Vui lòng sử dụng 'buy' hoặc 'sell'.")
+        print("Action không hợp lệ.")
 
 if __name__ == "__main__":
-    if len(sys.argv) < 4:
-        print("Usage: python bot.py [buy/sell] [amount] [token_address]")
+    if len(sys.argv) < 3:
+        print("Usage: python bot.py [buy/sell] [amount (optional)] [token_address]")
         sys.exit(1)
-
     action = sys.argv[1]
-    amount = float(sys.argv[2])
-    token_address = sys.argv[3]
+    if len(sys.argv) > 3: 
+        amount = float(sys.argv[2])
+        token_address = sys.argv[3]
+    else:
+        amount = None
+        token_address = sys.argv[2]
 
     asyncio.run(main(action, amount, token_address))
